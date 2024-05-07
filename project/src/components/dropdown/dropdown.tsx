843d94937a434cfb0be7150e38287a0e5ed76d9a
Olga <olga_bezrukova@bk.ru>
2024-05-08 00:26:01 +0300

    start

---
+++ project/src/components/dropdown/dropdown.tsx
@@ -0,0 +1,62 @@
+import  "../header/header.css";
+
+
+
+export const Navigation = () => {
+    return ( 
+<div className="wrapper_header_deskmenu">
+<div className="navigation">
+  <a href="#" className="link">New & Featured</a>
+  <ul className="navigation-content">
+    <li><a href="#">Limited Time</a></li>
+    <li><a href="#">Sale: Up to 50% Off</a></li>
+    <li><a href="#">Accessories</a></li>
+    <li><a href="#">Trending</a></li>
+  </ul>
+</div>
+<div className="navigation">
+  <a href="#">Men</a>
+  <ul className="navigation-content">
+    <li><a href="#">Shoes</a></li>
+    <li><a href="#">Shop by Sport</a></li>
+    <li><a href="#">Collections</a></li>
+  </ul>
+</div>
+<div className="navigation">
+  <a href="#">Kids</a>
+  <ul className="navigation-content">
+    <li><a href="#">Shoes</a></li>
+    <li><a href="#">Shop by Sport</a></li>
+    <li><a href="#">Collections</a></li>
+  </ul>
+</div>
+<div className="navigation">
+  <a href="#">Woman</a>
+  <ul className="navigation-content">
+    <li><a href="#">Shoes</a></li>
+    <li><a href="#">Shop by Sport</a></li>
+    <li><a href="#">Collections</a></li>
+  </ul>
+</div>
+<div className="navigation">
+  <a href="#">Sale</a>
+  <ul className="navigation-content">
+    <li><a href="#">Shoes</a></li>
+    <li><a href="#">Shop by Sport</a></li>
+    <li><a href="#">Collections</a></li>
+  </ul>
+</div>
+<div className="navigation">
+  <a href="#">Jordan</a>
+  <ul className="navigation-content">
+    <li><a href="#">Basketball</a></li>
+    <li><a href="#">Running</a></li>
+    <li><a href="#">Collections</a></li>
+    <li><a href="#">Gym & training</a></li>
+  </ul>
+</div>
+</div>
+
+);
+}
+export default Navigation
