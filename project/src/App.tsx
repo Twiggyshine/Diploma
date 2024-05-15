5234730e2d8c243256748e4f5c9591a43f77ba9f
Olga <olga_bezrukova@bk.ru>
2024-05-16 02:21:33 +0300

    layout

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -3,11 +3,19 @@ import Banner from "./components/banner/banner";
 import Title from "./components/title/title";
 import GridComponent from "./components/GridComponent/brand-item";
 import PicComponent from "./components/tranding/tranding";
+import Carousel from "./components/carousel/carousel";
 
 
+const App = () => {
+  const slides = [
+    <img src="/public/carousel/force.jpg" alt="Slide 1" className="carousel-img" />,
+    <img src="/public/carousel/nike-1.jpg" alt="Slide 2" className="carousel-img"/>,
+    <img src="/public/carousel/nike-2.jpg" alt="Slide 3" className="carousel-img" />,
+    <img src="/public/carousel/nike-3.jpeg" alt="Slide 4" className="carousel-img" />,
+    <img src="/public/carousel/nike-4.jpg" alt="Slide 5" className="carousel-img" />,
+    <img src="/public/carousel/nike-5.jpg" alt="Slide 6" className="carousel-img" />
+  ];
 
-
-function App() {
   return (
     <div className="container">
       <Header />
@@ -16,6 +24,7 @@ function App() {
       <GridComponent />
       <Title title="WE SERVE ATHLETES" subtitle="If you have a body,you are an athlete" />
       <PicComponent />
+      <Carousel slides={slides} />
     </div>
     
   );
---
+++ project/src/components/carousel/carousel.tsx
@@ -0,0 +1,41 @@
+
+import "./carousel.css";
+
+
+import React, { useState } from 'react';
+
+
+
+interface CarouselProps {
+  slides: JSX.Element[]; // Предполагается, что каждый элемент массива slides представляет собой контейнер с изображениями
+}
+
+const Carousel: React.FC<CarouselProps> = ({ slides }) => {
+  const [currentSlide, setCurrentSlide] = useState<number>(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === slides.length - 5 ? 0 : prevSlide + 1));
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prevSlide) => (prevSlide === 0 ? slides.length - 5 : prevSlide - 1));
+  };
+
+  return (
+    <div className="wrapper">
+    <div className="carousel">
+      <button className="prev" onClick={prevSlide}></button>
+      <div className="slides-container">
+        {slides.slice(currentSlide, currentSlide + 5).map((slide, index) => (
+          <div key={index} className="slide">
+            {slide}
+          </div>
+        ))}
+      </div>
+      <button className="next" onClick={nextSlide}></button>
+    </div>
+  </div>
+  );
+};
+
+export default Carousel;
