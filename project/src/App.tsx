487c3431b1ae62e3353e3efec9a115758e2db4c1
Olga <olga_bezrukova@bk.ru>
2024-05-16 15:06:33 +0300

    layout

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -4,17 +4,30 @@ import Title from "./components/title/title";
 import GridComponent from "./components/GridComponent/brand-item";
 import PicComponent from "./components/tranding/tranding";
 import Carousel from "./components/carousel/carousel";
+import  SHOP_DATA  from "./data/shoes.data";
+
+
+// const App = () => {
+//   const slides = [
+//     <img src="/public/carousel/nike-0.jpg" alt="Slide 1" className="carousel-img" />,
+//     <img src="/public/carousel/nike-1.jpg" alt="Slide 2" className="carousel-img"/>,
+//     <img src="/public/carousel/nike-2.jpg" alt="Slide 3" className="carousel-img" />,
+//     <img src="/public/carousel/nike-3.jpeg" alt="Slide 4" className="carousel-img" />,
+//     <img src="/public/carousel/nike-4.jpg" alt="Slide 5" className="carousel-img" />,
+//     <img src="/public/carousel/nike-5.jpg" alt="Slide 6" className="carousel-img" />
+//   ];
 
 
 const App = () => {
-  const slides = [
-    <img src="/public/carousel/force.jpg" alt="Slide 1" className="carousel-img" />,
-    <img src="/public/carousel/nike-1.jpg" alt="Slide 2" className="carousel-img"/>,
-    <img src="/public/carousel/nike-2.jpg" alt="Slide 3" className="carousel-img" />,
-    <img src="/public/carousel/nike-3.jpeg" alt="Slide 4" className="carousel-img" />,
-    <img src="/public/carousel/nike-4.jpg" alt="Slide 5" className="carousel-img" />,
-    <img src="/public/carousel/nike-5.jpg" alt="Slide 6" className="carousel-img" />
-  ];
+  const nikeItems = SHOP_DATA.nike.items;
+
+  const slides = nikeItems.map(item => (
+    <div key={item.id} className="slide-content">
+      <img src={`/public/carousel/${item.image}`} alt={item.name} className="carousel-img" />
+      <h3>{item.name}</h3>
+      <p>${item.price}</p>
+    </div>
+  ));
 
   return (
     <div className="container">
---
+++ project/src/data/shoes.data.tsx
@@ -0,0 +1,47 @@
+const SHOP_DATA = {
+    nike: {
+      id: 1,
+      title: 'NIKE',
+      routeName: 'nike',
+      items: [
+        {
+          id: 2,
+          name: ' Air Force 1',
+          image: 'nike-0.jpg',
+          price: 150
+        },
+        {
+          id: 3,
+          name: 'Air Zoom Pegasus 37',
+          image: 'nike-1.jpg',
+          price: 150
+        },
+        {
+          id: 4,
+          name: 'P 6000',
+          image: 'nike-2.jpg',
+          price: 180
+        },
+        {
+          id: 5,
+          name: 'Presto Fly',
+          image: 'nike-3.jpg',
+          price: 96,
+          oldPrice: 120
+        },
+        {
+          id: 6,
+          name: 'Air Max 87',
+          image: 'nike-4.png',
+          price: 110
+        },
+        {
+            id: 7,
+            name: 'Dunk Low',
+            image: 'nike-5.png',
+            price: 110
+          },
+      ]
+    }
+}
+export default SHOP_DATA
