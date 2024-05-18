4aaa749c576825483d81c9ec11bf72971e3426b7
Olga <olga_bezrukova@bk.ru>
2024-05-18 15:20:13 +0300

    layout

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -5,6 +5,8 @@ import GridComponent from "./components/GridComponent/brand-item";
 import PicComponent from "./components/tranding/tranding";
 import Carousel from "./components/carousel/carousel";
 import SHOP_DATA  from "./data/shoes.data";
+import Footer from "./components/footer/footer";
+
 
 
 
@@ -28,6 +30,7 @@ const App = () => {
       <Title title="WE SERVE ATHLETES" subtitle="If you have a body,you are an athlete" />
       <PicComponent />
       <Carousel slides={slides} />
+      <Footer />
     </div>
     
   );
