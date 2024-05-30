d8287e92172d0d3e3c3e0ee8f5de87abd38b7067
Olga <olga_bezrukova@bk.ru>
2024-05-30 13:06:55 +0300

    layout

--- project/src/pages/HomePage.tsx
+++ project/src/pages/HomePage.tsx
@@ -17,7 +17,6 @@ const HomePage = () => {
     <div key={item.id} className="slide-content">
       <img src={`/public/carousel/${item.image}`} alt={item.name} className="carousel-img" />
       <h3>{item.name}</h3>
-      <p>${item.price}</p>
     </div>
   ));
 
